5d175a7b3be947b428cb1764e63adbc0441d975d
Stefan Ludwig <github@slu-it.de>
2024-09-30 23:08:41 +0200

    initial setup

---
+++ src/main/kotlin/examples/SpringAiTestApplication.kt
@@ -0,0 +1,11 @@
+package examples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAiTestApplication>(*args)
+}
---
+++ src/test/kotlin/examples/SpringAiTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package examples
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
